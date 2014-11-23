7b2aa978ca2ceca65dbb98f4cd316b3766c5141e
lazyarea <k4zuhisa@gmail.com>
2014-11-24 05:51:52 +0900

    modified 24 nov 20014

--- Chemicals/lib/Chemicals/Controller/Root.pm
+++ Chemicals/lib/Chemicals/Controller/Root.pm
@@ -4,42 +4,13 @@ use namespace::autoclean;
 
 BEGIN { extends 'Catalyst::Controller' }
 
-#
-# Sets the actions in this controller to be registered with no prefix
-# so they function identically to actions created in MyApp.pm
-#
 __PACKAGE__->config(namespace => '');
 
-=encoding utf-8
-
-=head1 NAME
-
-Chemicals::Controller::Root - Root Controller for Chemicals
-
-=head1 DESCRIPTION
-
-[enter your description here]
-
-=head1 METHODS
-
-=head2 index
-
-The root page (/)
-
-=cut
 
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
-
-    # Hello World
-    $c->response->body( $c->welcome_message );
+    $c->stash->{template} = 'index.tt';
 }
-
-=head2 default
-
-Standard 404 error page
-
-=cut
 
 sub default :Path {
     my ( $self, $c ) = @_;
@@ -47,24 +18,99 @@ sub default :Path {
     $c->response->status(404);
 }
 
-=head2 end
+#-----------------------------------------------------------
+sub blog :Local
+#-----------------------------------------------------------
+{
+    my ($self,$c) = @_;
+    $c->stash->{template} = 'blog.tt';
+}
+#-----------------------------------------------------------
 
-Attempt to render a view, if needed.
+#-----------------------------------------------------------
+sub blog_single :Local
+#-----------------------------------------------------------
+{
+    my ($self,$c) = @_;
+    $c->stash->{template} = 'blog_single.tt';
+}
+#-----------------------------------------------------------
 
-=cut
+#-----------------------------------------------------------
+sub contact :Local
+#-----------------------------------------------------------
+{
+    my ($self,$c) = @_;
+    $c->stash->{template} = 'contact.tt';
+}
+#-----------------------------------------------------------
+
+#-----------------------------------------------------------
+sub galleries :Local
+#-----------------------------------------------------------
+{
+    my ($self,$c) = @_;
+    $c->stash->{template} = 'galleries.tt';
+}
+#-----------------------------------------------------------
+
+#-----------------------------------------------------------
+sub pinterest_style :Local
+#-----------------------------------------------------------
+{
+    my ($self,$c) = @_;
+    $c->stash->{template} = 'pinterest-style.tt';
+}
+#-----------------------------------------------------------
+
+#-----------------------------------------------------------
+sub portfolio :Local
+#-----------------------------------------------------------
+{
+    my ($self,$c) = @_;
+    $c->stash->{template} = 'portfolio.tt';
+}
+#-----------------------------------------------------------
+
+#-----------------------------------------------------------
+sub pricing_table :Local
+#-----------------------------------------------------------
+{
+    my ($self,$c) = @_;
+    $c->stash->{template} = 'pricing-table.tt';
+}
+#-----------------------------------------------------------
+
+#-----------------------------------------------------------
+sub product_single :Local
+#-----------------------------------------------------------
+{
+    my ($self,$c) = @_;
+    $c->stash->{template} = 'product-single.tt';
+}
+#-----------------------------------------------------------
+
+#-----------------------------------------------------------
+sub products_pageproduct_single :Local
+#-----------------------------------------------------------
+{
+    my ($self,$c) = @_;
+    $c->stash->{template} = 'products-page.tt';
+}
+#-----------------------------------------------------------
+
+#-----------------------------------------------------------
+sub tiles :Local
+#-----------------------------------------------------------
+{
+    my ($self,$c) = @_;
+    $c->stash->{template} = 'tiles.tt';
+}
+#-----------------------------------------------------------
+
 
 sub end : ActionClass('RenderView') {}
 
-=head1 AUTHOR
-
-Charlie &
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
 
 __PACKAGE__->meta->make_immutable;
 
